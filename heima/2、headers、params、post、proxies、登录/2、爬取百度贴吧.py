9a309d8730926b868f387a92b27bc2a5ea4fde57
xws <xws>
2019-06-12 10:06:50 +0800

    爬取百度贴吧

--- heima/2、headers、params、post、proxies、登录/2、爬取百度贴吧.py
+++ heima/2、headers、params、post、proxies、登录/2、爬取百度贴吧.py
@@ -7,11 +7,14 @@ class Tieba:
         self.url_temp="http://tieba.baidu.com/f?kw=" + tieba_name + "&ie=utf-8&pn={}"
         self.headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36"}
 
+    #构造url列表
     def get_url_list(self):
         url_list=[]
         for i in range(1000):
             url_list.append(self.url_temp.format(i*50))
         return url_list
+
+        # return [self.url_temp.format(i * 50) for i in range(1000)]
 
 
     #发送请求，获取响应
@@ -24,6 +27,7 @@ class Tieba:
     #保存html字符串
     def save_html(self,html_str,page_num):
         file_path="{},第{}页.html".format(self.tieba_name,page_num)
+        print("测试")
         with open(file_path,"w",encoding="utf-8") as f:
             f.write(html_str)
 
@@ -34,22 +38,13 @@ class Tieba:
         #2、遍历、发送请求、获取响应
         for url in url_list:
             html_str=self.parse_url(url)
-        #3、保存
-        page_num=url_list.index(url)+1    #页码数
-        self.save_html(html_str,page_num)
+            #3、保存
+            #获取url列表中第一个元素的位置，真实的页码数为：元素位置+1
+            page_num=url_list.index(url)+1
+            self.save_html(html_str,page_num)
 
-# if __name__ == '__main__':
-#     tieba=Tieba("计算机")
-#     tieba.run()
-
-
-
+if __name__ == '__main__':
+    tieba=Tieba("计算机")
+    tieba.run()
 
 
-
-
-
-
-tieba_name="xws"
-url="http://tieba.baidu.com/f?kw="+tieba_name+"&ie=utf-8&pn=50"
-print(url)
