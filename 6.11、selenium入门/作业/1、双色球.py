3da313a139125030946562a8db6ebcbac21ac3d1
xws <xws>
2019-06-21 16:25:32 +0800

    更新

--- 6.11、selenium入门/作业/1、双色球.py
+++ 6.11、selenium入门/作业/1、双色球.py
@@ -1,10 +1,9 @@
 import requests
 from lxml import etree
 
-
 url="http://zst.aicai.com/ssq/openInfo/"
 
-# 请求url地址,返回 <Response [200]>
+# 请求url地址
 response=requests.get(url)
 
 # 将请求的网页解码为utf-8
@@ -16,9 +15,7 @@ root=etree.HTML(html)
 result=root.xpath("//tbody/tr[@onmouseout]")
 
 for i in result:
-    res = i.xpath("./td/text()")
-    print(res)
-
-
-
-
+    res=i.xpath("./td")
+    for j in res:
+        a=j.xpath("./text()")[0]
+        print(a)
